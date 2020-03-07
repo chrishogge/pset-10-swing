e3c11e26d6f0c647cfbc9f3bfaf799196181c66f
Admin <chogge@ucvts.org>
2020-03-07 15:26:50 -0500

    Started implementing the insertion of a new word into the json

--- src/wordEditor.java
+++ src/wordEditor.java
@@ -15,8 +15,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Vector;
-
 import javax.swing.SpringLayout;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.JScrollPane;
 import java.awt.BorderLayout;
 import com.jgoodies.forms.layout.FormLayout;
@@ -81,8 +82,7 @@ public class wordEditor {
 		JLabel lblWord = new JLabel("Word:");
 		frame.getContentPane().add(lblWord, "flowx,cell 0 0");
 		
-		JButton btnNewButton = new JButton("Create Word");
-		frame.getContentPane().add(btnNewButton, "cell 4 0");
+		
 		
 		JLabel lblSynonyms = new JLabel("Synonyms:");
 		frame.getContentPane().add(lblSynonyms, "flowx,cell 0 1");
@@ -98,14 +98,17 @@ public class wordEditor {
 		textField_8 = new JTextField();
 		frame.getContentPane().add(textField_8, "cell 0 0");
 		textField_8.setColumns(10);
+		textField_8.setName("textField");
 		
 		txtWordWordWord = new JTextField();
 		frame.getContentPane().add(txtWordWordWord, "cell 0 1");
 		txtWordWordWord.setColumns(10);
+		txtWordWordWord.setName("textField");
 		
 		textField_9 = new JTextField();
 		frame.getContentPane().add(textField_9, "cell 0 2");
 		textField_9.setColumns(10);
+		textField_9.setName("textField");
 		
 		JScrollPane scrollPane = new JScrollPane();
 		frame.getContentPane().add(scrollPane, "cell 0 4,grow");
@@ -127,16 +130,25 @@ public class wordEditor {
 		button_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				createDefs(frame, i, panel);
+				createDefs(i, panel);
 				i += 2;
 				frame.validate();
 			}
 		});
 		frame.getContentPane().add(button_1, "cell 0 3");
 		
+		JButton btnNewButton = new JButton("Create Word");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				addWord();
+			}
+		});
+		frame.getContentPane().add(btnNewButton, "cell 4 0");
+		
 	}
 	
-	public void createDefs(JFrame frame, int i, JPanel panel) {
+	public void createDefs(int i, JPanel panel) {
 //		JPanel panel = new JPanel();
 //		scrollPane.add(scrollPane, "cell 0 " + Integer.toString(i) + ",grow");
 //		panel.setLayout(new MigLayout("", "[][][][][][]", "[][]"));
@@ -162,8 +174,75 @@ public class wordEditor {
 	    for (Component comp : comps) {
 	        compList.add(comp);
 	        if (comp instanceof Container)
+	        
 	            compList.addAll(getAllComponents((Container) comp));
 	    }
 	    return compList;
+	}
+	
+	public void addWord() {
+		
+		List<Component> comps = getAllComponents(frame);
+		
+		ArrayList<String> textFields = new ArrayList<String>();
+		ArrayList<String> partsOfSpeech = new ArrayList<String>();
+		
+		for(i = 0; i < comps.size(); i++){
+			if(comps.get(i).getName() != null) {
+				if(comps.get(i).getName().equals("textField")) {
+					JTextField temp = (JTextField) comps.get(i);
+					textFields.add(temp.getText());
+				}
+			}
+		}
+		
+		System.out.println(textFields);
+		
+		for(i = 0; i < comps.size(); i++){
+			if(comps.get(i).getName() != null) {
+				if(comps.get(i).getName().equals("comboBox")) {
+					JComboBox temp = (JComboBox) comps.get(i);
+					String tempString = (String)temp.getSelectedItem();
+					partsOfSpeech.add(tempString);
+					
+				}
+			}
+		}
+		
+		ArrayList<String> fullWord = new ArrayList<String>();
+		fullWord.add(textFields.get(0));
+		fullWord.add(textFields.get(1));
+		fullWord.add(textFields.get(2));
+		ArrayList<Definition> defs = new ArrayList<Definition>();
+		
+		for(int i = 0; i < partsOfSpeech.size(); i++) {
+			defs.add(new Definition(textFields.get(3+i), partsOfSpeech.get(i)));
+		}
+		
+		ArrayList<Word> words = Dictionary.getWords();
+		
+		String word = fullWord.get(0);
+		String[] syns = textFields.get(1).split(", ");
+		ArrayList<String> synsArray = new ArrayList<String>();
+		for(int i = 0; i < syns.length-1; i++) {
+			synsArray.add(syns[i]);
+		}
+		
+		String[] ants = textFields.get(2).split(", ");
+		ArrayList<String> antsArray = new ArrayList<String>();
+		for(int i = 0; i < syns.length-1; i++) {
+			antsArray.add(ants[i]);
+		}
+		
+		System.out.println(Arrays.deepToString(syns));
+		System.out.println(Arrays.deepToString(ants));
+		
+		Word newWord = new Word(fullWord.get(0),defs,synsArray,antsArray);
+		
+		words.add(newWord);
+		
+		Dictionary.editJson(words);
+		
+		
 	}
 }
